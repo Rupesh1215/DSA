daf50cd1a9be3a899cd5b53164630d8db7332cbe
Rupesh <xyz@gmail.com>
2025-12-24 11:23:31 +0530

    Valid Anagram Problem

---
+++ Arrays/ValidAnagram242.java
@@ -0,0 +1,35 @@
+package Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String s = scanner.nextLine();
+        System.out.print("Enter the second string: ");
+        String t = scanner.nextLine();
+        System.out.println(solution.isAnagram(s, t));
+        scanner.close();
+    }
+}
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int []charCount = new int[26];
+        for(int i=0; i<s.length();i++){
+            charCount[s.charAt(i) - 'a']++;
+            charCount[t.charAt(i) - 'a']--;
+        }
+
+        for(int count : charCount){
+            if(count != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
